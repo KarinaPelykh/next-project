5c16b7079b62ee74b610b6c80427c2840c23ab6b
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2024-06-04 20:39:56 +0200

    setting

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import { lusitana } from "./ui/fonts";
+export default function Home() {
+  return (
+    <>
+      <p
+        className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+      >
+        <strong>Welcome to Acme.</strong> This is the example for the{" "}
+        <a href="https://nextjs.org/learn/" className="text-blue-500">
+          Next.js Learn Course
+        </a>
+        , brought to you by Vercel.
+      </p>
+    </>
+  );
+}
